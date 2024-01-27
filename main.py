6c2903104bba4d631a71e08062a10791ac45b755
IrynaBarnych <iryna.barnych@gmail.com>
2024-01-27 19:35:31 +0200

    Завдання 2 Додайте до першого завдання можливість вносити, видаляти, оновлювати дані за допомогою запитів INSERT, DELETE, UPDATE. Перед виконанням запиту перевіряйте правильність назви таблиці. Також забороніть запит на видалення та оновлення усіх рядків (UPDATE та DELETE без умов).

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-# Створіть однотабличну базу даних People (ім’я, прізвище, місто, країна, дата народження) з однойменною
-# таблицею. Напишіть програму, яка дозволяє користувачеві ввести запит і отримати результати роботи запиту.
-# Підтримуйте лише SELECT як запит. Якщо ви спробуєте
-# виконати інші запити, потрібно буде генерувати помилку.
+# Завдання 2
+# Додайте до першого завдання можливість вносити,
+# видаляти, оновлювати дані за допомогою запитів INSERT,
+# DELETE, UPDATE. Перед виконанням запиту перевіряйте
+# правильність назви таблиці. Також забороніть запит на
+# видалення та оновлення усіх рядків (UPDATE та DELETE
+# без умов).
 
-from sqlalchemy import create_engine, Column, Integer, String, Sequence, Date
-from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy.sql import text
-
+from sqlalchemy import create_engine, MetaData, Table, insert, update, delete
+from sqlalchemy.sql import select
+import psycopg2
 import json
 
 # Зчитування конфігураційних даних з файлу
@@ -19,45 +21,101 @@ db_password = config['password']
 
 db_url = f'postgresql+psycopg2://{db_user}:{db_password}@localhost:5432/People'
 engine = create_engine(db_url)
+# з'єднання з БД
+conn = engine.connect()
+metadata = MetaData()
+# завантаження таблиць
+# автоматичне завантаження
+metadata.reflect(bind=engine)
+# або одна табличка
+#departments = Table('departments', metadata, autoload=True, autoload_with=engine)
 
-#оголошення базового класу
-Base = declarative_base()
-#визначення класу моделі
-class Person(Base):
-    __tablename__ = 'people'
-    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
-    first_name = Column(String(50))
-    last_name = Column(String(50))
-    city = Column(String(50))
-    country = Column(String(50))
-    birth_date = Column(Date)
+def insert_row(table: metadata):
+    columns = table.columns.keys()
 
-#створення таблиці
-Base.metadata.create_all(engine)
+    values = {}
+    for columns in columns:
+        value = input(f"Введіть значення для колонки {columns}")
+        values[columns] = value
+    query = insert(table).values(values)
+    conn.execute(query)
+    conn.commit()
 
-#створення сесії та додавання запису
-Session = sessionmaker(bind=engine)
-session = Session()
-#додавання інформації
-person1 = Person(first_name='John', last_name='Doe', city='New York', country='USA', birth_date='1990-01-15')
-person2 = Person(first_name='Jane', last_name='Smith', city='London', country='UK', birth_date='1985-03-22')
+    print("Рядок успішно додано!")
 
-session.add_all([person1, person2])
-session.commit()
-#програма для виконання select-запитів
+def update_rows(table):
+    columns = table.columns.keys()
+    print("Доступні колонки для оновлення: ")
+    for idx, column in enumerate(columns, start=1):
+        print(f"{idx}.{column}")
+    selected_column_idx = int(input("ВВедіть номер колонки для оновлення: "))
+
+    if 1 <= selected_column_idx <= len(columns):
+        condition_column = columns[selected_column_idx - 1]
+    else:
+        print("невірний номер колонки!")
+
+    condition_value = input("ВВедіть значення для умови, {condition_column}: ")
+    new_values = {}
+    for column in columns:
+        value = input(f"ВВедіть значення для колонки {column}: ")
+        new_values[column] = value
+
+    confirm_update = input("Оновити усі рядки? у/п? ")
+    if confirm_update.lower() == 'y':
+        query = update(table).where(getattr(table.c, condition_column) == condition_value).values(new_values)
+        conn.execute(query)
+        conn.commit()
+
+
+def delete_rows(table):
+    columns = table.columns.keys()
+    print("Доступні колонки для видалення: ")
+    for idx, column in enumerate(columns, start=1):
+        print(f"{idx}.{column}")
+    selected_column_idx = int(input("ВВедіть номер колонки для умови видалення: "))
+
+    if 1 <= selected_column_idx <= len(columns):
+        condition_column = columns[selected_column_idx - 1]
+    else:
+        print("невірний номер колонки! Видалення відмінено!")
+
+    condition_value = input("ВВедіть значення для умови, {condition_column}: ")
+
+    confirm_update = input("Видалити усі рядки з цієї таблиці? у/п? ")
+    if confirm_update.lower() == 'y':
+        query = delete(table).where(getattr(table.c, condition_column) == condition_value)
+        conn.execute(query)
+        conn.commit()
+
 while True:
-    user_query = input("Введіть вас select-запит exit  - вихід")
-    if user_query.lower() == "exit":
+    print("Оберіть таблицю: ")
+    for table_name in metadata.tables.keys():
+        print(table_name)
+    table_name = input("Введіть назву таблиці або 0, щоб вийти ")
+    if table_name == '0':
         break
-    try:
-        result = session.execute(text(user_query))
-        rows = result.fetchall()
-        if rows:
-            print("Результат: ")
-            for row in rows:
-                print(row)
+    # перевіримо, чи існує таблиця
+    if table_name in metadata.tables:
+        table = metadata.tables[table_name]
+        print(f"Ви обрали таблицю {table_name}")
+
+        print("1. Вставити рядки")
+        print("2. Оновити рядки")
+        print("3. Видалити рядки")
+        print("0. Вийти")
+
+        choice = input("Оберіть опцію: ")
+
+        if choice == "1":
+            insert_row(table)
+        elif choice == "2":
+            update_rows(table)
+        elif choice == "3":
+            delete_rows(table)
+        elif choice == "0":
+            break
         else:
-            print("Результат відсутній")
-    except Exception as e:
-        print(f"Помилка виконання запиту: {e}")
-session.close()
+            print("Невірний вибір. Будь ласка, оберіть знову.")
+    else:
+        print("Такої таблиці не існує. Будь ласка, введіть правильну назву.")
